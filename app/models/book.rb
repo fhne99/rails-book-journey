a4d15662035781fe3f47a949e747ce59f113195a
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-05-21 16:04:36 +0200

    book model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
-  
+  validates :title, presence: true, uniqueness: true
+  validates :author, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :comment, length: { maximum: 500 }
 end
