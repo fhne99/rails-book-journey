58f7bad7c886f86ae1182e3d251483b83fe511ac
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-05-30 16:46:46 +0200

    icons for reading status done

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,14 @@
 module BooksHelper
+  def status_icon(book)
+    case book.status
+    when 'lecture_en_attente'
+      content_tag(:i, '', class: 'fa-regular fa-circle-xmark')
+    when 'lecture_en_cours'
+      content_tag(:i, '', class: 'fa-regular fa-clock')
+    when 'lecture_terminée'
+      content_tag(:i, '', class: 'fa-regular fa-circle-check')
+    else
+      content_tag(:i, '', class: 'fa fa-question status-icon default')
+    end
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,10 @@ class Book < ApplicationRecord
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :comment, length: { maximum: 500 }
 
-  enum status: { not_started: "Not started", in_progress: "In progress", finished: "Finished" }
-  validates :status, inclusion: { in: statuses.keys }
+  STATUSES = {
+    not_started: "lecture_en_attente",
+    in_progress: "lecture_en_cours",
+    finished: "lecture_terminée"
+}.freeze
+  validates :status, inclusion: { in: STATUSES.values }, presence: true
 end
