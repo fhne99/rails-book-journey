243ef150bab3427c260bf7bc005deb3d30566958
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-06-07 22:31:41 +0200

    color reading status icons

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,13 +2,13 @@ module BooksHelper
   def status_icon(book)
     case book.status
     when 'lecture_en_attente'
-      content_tag(:i, '', class: 'fa-regular fa-circle-xmark')
+      content_tag(:i, '', class: 'fa-regular fa-circle-xmark status-icon')
     when 'lecture_en_cours'
-      content_tag(:i, '', class: 'fa-regular fa-clock')
+      content_tag(:i, '', class: 'fa-regular fa-clock status-icon')
     when 'lecture_terminée'
-      content_tag(:i, '', class: 'fa-regular fa-circle-check')
+      content_tag(:i, '', class: 'fa-regular fa-circle-check status-icon')
     else
-      content_tag(:i, '', class: 'fa fa-question status-icon default')
+      content_tag(:i, '', class: 'fa fa-question status-icon default status-icon')
     end
   end
 end
