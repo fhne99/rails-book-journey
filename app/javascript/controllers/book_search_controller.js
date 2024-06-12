8ec767b6095060ba6515702592b242eba12612b6
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-06-12 12:54:55 +0200

    fetch started in stimulus controller

--- app/javascript/controllers/book_search_controller.js
+++ app/javascript/controllers/book_search_controller.js
@@ -4,6 +4,17 @@ export default class extends Controller {
   static targets = ["query", "results"]
 
   connect() {
-    console.log(this.queryTarget, this.resultsTarget)
+    console.log("book search controller connected")
+  }
+
+  search(event) {
+    event.preventDefault()
+    const query = this.queryTarget.value
+
+    fetch(`https://openlibrary.org/search.json?q=${query}`)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
   }
 }
